29c98955c6028c4cc4fd1e2bbffa3721b138e2cd
Felix Ruess <felix.ruess@gmail.com>
2013-03-02 01:02:36 +0100

    [tests] ms2100 magnetometer test prog

--- sw/airborne/lisa/test/lisa_test_ms2100.c
+++ sw/airborne/lisa/test/lisa_test_ms2100.c
@@ -19,25 +19,19 @@
  * Boston, MA 02111-1307, USA.
  */
 
-#include <stm32/gpio.h>
-#include <stm32/flash.h>
-#include <stm32/misc.h>
-#include <stm32/spi.h>
-#include <stm32/dma.h>
-#include <stm32/exti.h>
 
 #include BOARD_CONFIG
 #include "mcu.h"
 #include "mcu_periph/sys_time.h"
+#include "mcu_periph/uart.h"
 #include "subsystems/datalink/downlink.h"
+//#include "messages.h"
 #include "peripherals/ms2100.h"
 #include "led.h"
 
 static inline void main_init( void );
 static inline void main_periodic_task( void );
 static inline void main_event_task( void );
-
-static inline void main_spi2_init(void);
 
 int main(void) {
   main_init();
@@ -55,68 +49,34 @@ int main(void) {
 static inline void main_init( void ) {
   mcu_init();
   sys_time_register_timer((1./PERIODIC_FREQUENCY), NULL);
-  ms2100_init();
-  main_spi2_init();
+
+  ms2100_init(&ms2100, &(MS2100_SPI_DEV), MS2100_SLAVE_IDX);
+
+  mcu_int_enable();
 }
 
 static inline void main_periodic_task( void ) {
   RunOnceEvery(10,
-	       {
-		 DOWNLINK_SEND_BOOT(DefaultChannel, DefaultDevice, &sys_time.nb_sec);
-		 LED_PERIODIC();
-	       });
+               {
+                 DOWNLINK_SEND_BOOT(DefaultChannel, DefaultDevice, &sys_time.nb_sec);
+                 LED_PERIODIC();
+                 LED_TOGGLE(2)
+               });
 
-  switch(ms2100_status) {
-  case MS2100_IDLE:
-    Ms2100SendReq();
-    break;
-  case MS2100_WAITING_EOC:
-    if (Ms2100HasEOC()) {
-      Ms2100ReadRes();
-    }
-    break;
-  }
+  ms2100_periodic(&ms2100);
+
 }
 
 static inline void main_event_task( void ) {
-  if (ms2100_status == MS2100_DATA_AVAILABLE) {
+  ms2100_event(&ms2100);
+  if (ms2100.status == MS2100_DATA_AVAILABLE) {
     RunOnceEvery(10, {
-	DOWNLINK_SEND_IMU_MAG_RAW(DefaultChannel, DefaultDevice,
-				  &ms2100_values[0],
-				  &ms2100_values[1],
-				  &ms2100_values[2]);
+        DOWNLINK_SEND_IMU_MAG_RAW(DefaultChannel, DefaultDevice,
+                                  &ms2100.data.vect.x,
+                                  &ms2100.data.vect.y,
+                                  &ms2100.data.vect.z);
       });
-    ms2100_status = MS2100_IDLE;
+    ms2100.status = MS2100_IDLE;
   }
-}
-
-static inline void main_spi2_init( void ) {
-
-  /* set max1168 slave select as output and assert it ( on PB12) */
-  GPIOB->BSRR = GPIO_Pin_12;
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-  GPIO_InitTypeDef GPIO_InitStructure;
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GPIOB, &GPIO_InitStructure);
-
-  /* Enable SPI2 Periph clock -------------------------------------------------*/
-  RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI2, ENABLE);
-
-  /* Configure GPIOs: SCK, MISO and MOSI  --------------------------------*/
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-  GPIO_Init(GPIOB, &GPIO_InitStructure);
-
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB | RCC_APB2Periph_AFIO , ENABLE);
-
-
-  /* Enable SPI_2 DMA clock ---------------------------------------------------*/
-  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1, ENABLE);
-
-
-
 }
 
